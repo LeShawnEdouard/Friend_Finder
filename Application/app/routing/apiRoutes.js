5a52fe9264c13eaee8f635eb8f1a53386e2c92be
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-05-11 18:23:40 -0400

    Added GET and POST route to the apoRoutes.js file

---
+++ Application/app/routing/apiRoutes.js
@@ -0,0 +1,28 @@
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendsData = require("../data/friendsData");
+
+
+module.exports = function(app) {
+
+    
+    // ---------------------------------------------------------------------------
+  
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+    });
+  
+
+    // ---------------------------------------------------------------------------
+  
+  app.post("/api/friends", function(req, res) {
+
+    if (friendsData.length < 5) {
+    friendsData.push(req.body);
+    res.json(true);
+    }
+  });
+};
