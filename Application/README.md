b0da5f1b9cd94f63467b19bd11c103eda17c8676
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-05-17 21:55:00 -0400

    Finished adding comments and completed the README

--- Application/README.md
+++ Application/README.md
@@ -1,1 +1,9 @@
 # Friend_Finder
+
+Welcome to the Friend Finder Application. Here you will find your new best friend.
+
+Instructions:
+
+1. On the Home page click "Go to Survey" to start the survey.
+2. Enter your first and last name with an image of you.
+3. Answer the following 10 questions to the best of your ability and click "Submit".
--- Application/app/routing/htmlRoutes.js
+++ Application/app/routing/htmlRoutes.js
@@ -1,3 +1,4 @@
+// Path varialbe used to get the correct file path for our html
 var path = require("path");
 
 
@@ -5,11 +6,12 @@ var path = require("path");
 
 module.exports = function(app) {
 
-
+  // GET request used to handle when a user visits the page
   app.get("/survey", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
+  // This is a default if no matching route is undefined
   app.get("*", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/home.html"));
   });
