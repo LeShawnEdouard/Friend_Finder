33f11619c0e338d52d7039ff97459adb8b4c2908
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-01-29 21:03:58 -0500

    Changed photos for each friend in the friends list array

--- Application/app/data/friends.js
+++ Application/app/data/friends.js
@@ -3,7 +3,7 @@
 var friendsList = [
     {
         "name":"Robert Downey Jr.",
-        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbB8mYTj1I3l7Kq1peD8EH1FxFfz0b3J0kRPZfDmr59PhQmOfe",
+        "photo":"https://i.insider.com/5b52400e51dfbe20008b45f9?width=1100&format=jpeg&auto=webp",
         "scores":[
             1,
             5,
@@ -19,7 +19,7 @@ var friendsList = [
       },
       {
         "name":"Chris Evans",
-        "photo":"https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F180830150855-chris-evans.jpg",
+        "photo":"https://cdn.vox-cdn.com/thumbor/Z4yyRrPenLpg0ExhHpetrveJiuY=/0x0:2034x1356/1200x800/filters:focal(0x0:2034x1356)/cdn.vox-cdn.com/uploads/chorus_image/image/46293074/Avengers254d1158022f16-2040.0.0.jpg",
         "scores":[
             4,
             2,
@@ -35,7 +35,7 @@ var friendsList = [
       },
       {
         "name":"Tom Holland",
-        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlkudJHIgzEVKegMBNA7d5LPH5a783RhU5tbw4wwblHxP9Z5fXzg",
+        "photo":"https://cdn.vox-cdn.com/thumbor/3LI5EGLK1lbgApqkw06rr_x2h5U=/0x0:655x365/1200x800/filters:focal(249x23:353x127)/cdn.vox-cdn.com/uploads/chorus_image/image/55356661/spider_man_homecoming.0.jpg",
         "scores":[
             5,
             1,
@@ -51,7 +51,7 @@ var friendsList = [
       },
       {
         "name":"Josh Brolin",
-        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVSwHui9QMVTHajrXxcfF-Nv_6SHkzKRnofEdM5qu42eMeGoRtiQ",
+        "photo":"https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/03/20/Pictures/_97442748-2c12-11e8-8732-87a46da2a8cc.JPG",
         "scores":[
             1,
             1,
@@ -67,7 +67,7 @@ var friendsList = [
       },
       {
         "name":"Don Cheadle",
-        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuc895apc5I_NeOzhzINjAaCZ5DdYlOKPleGYHRlsXzzrF2O-O6A",
+        "photo":"https://mcuexchange.com/wp-content/uploads/2019/04/8-WarMachine.jpg",
         "scores":[
             5,
             2,
